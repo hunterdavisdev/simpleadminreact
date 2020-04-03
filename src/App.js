8ca17cc1d75dffe3c49388b48135c2e28de5fb8c
Hunter Davis <developer.hunterdavis@gmail.com>
2020-04-02 21:20:29 -0500

    Added grid rows

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 /* global chrome*/
-import React from 'react';
-import Navbar from './components/Navbar';
-import Search from './components/Search';
-import Tools from './components/Tools';
-import Account from './components/Account';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import { Grid, Header } from 'semantic-ui-react';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Search from "./components/Search";
+import Tools from "./components/Tools";
+import Account from "./components/Account";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import { Grid, Header } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   React.useEffect(() => {
-    chrome.tabs.query({ url: '*://admin.simplechurchcrm.com/*' }, results => {
+    chrome.tabs.query({ url: "*://admin.simplechurchcrm.com/*" }, results => {
       if (results.length === 0) return;
-      let port = chrome.tabs.connect(results[0].id, { name: 'sccrm' });
-      port.postMessage({ msg: 'Hello from React!' });
+      let port = chrome.tabs.connect(results[0].id, { name: "sccrm" });
+      port.postMessage({ msg: "Hello from React!" });
       port.onMessage.addListener(msg => {
         console.log(msg);
       });
@@ -21,19 +26,34 @@ function App() {
   });
 
   return (
-    <div className='App'>
+    <div className="App">
       <Router forceRefresh={false}>
-        <Grid columns={2} style={{ height: '100%', margin: 0 }}>
-          <Grid.Column floated='left' width={4}>
-            <Navbar />
-          </Grid.Column>
-          <Grid.Column floated='right' width={12}>
-            <Switch>
-              <Route path='/tools' component={Tools} />
-              <Route path='/account' component={Account} />
-              <Route path='*' component={Search} />
-            </Switch>
-          </Grid.Column>
+        <Grid style={{ height: "100%", margin: 0 }}>
+          <Grid.Row columns={1}>
+            <Grid.Column style={{ backgroundColor: "red" }}>
+              <Header as="h2">Simple Admin</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column
+              floated="left"
+              width={4}
+              style={{ backgroundColor: "green" }}
+            >
+              <Navbar />
+            </Grid.Column>
+            <Grid.Column
+              floated="right"
+              width={12}
+              style={{ backgroundColor: "yellow" }}
+            >
+              <Switch>
+                <Route path="/tools" component={Tools} />
+                <Route path="/account" component={Account} />
+                <Route path="*" component={Search} />
+              </Switch>
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
       </Router>
     </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Dropdown, Input } from 'semantic-ui-react';
+import React from "react";
+import { Dropdown, Input } from "semantic-ui-react";
 
 const options = [
-  { key: 'Name', text: 'Name', value: 'Name' },
-  { key: 'ID', text: 'ID', value: 'ID' },
-  { key: 'Email', text: 'Email', value: 'Email' },
-  { key: 'Domain', text: 'Domain', value: 'Domain' }
+  { key: "Name", text: "Name", value: "Name" },
+  { key: "ID", text: "ID", value: "ID" },
+  { key: "Email", text: "Email", value: "Email" },
+  { key: "Domain", text: "Domain", value: "Domain" }
 ];
 const Search = () => {
   return (
     <Input
       fluid
-      size='small'
+      size="small"
       loading
-      label={<Dropdown size='small' inverted defaultValue='Name' options={options} />}
-      labelPosition='left'
-      placeholder='Start typing'
+      label={
+        <Dropdown size="small" inverted defaultValue="Name" options={options} />
+      }
+      labelPosition="left"
+      placeholder="Start typing"
     />
   );
 };
