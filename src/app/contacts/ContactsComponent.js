77b0fb235f7dd53aae1c8d04984d6031cc496807
hunterdavisdev <hunterdavis.lua@gmail.com>
2020-07-04 17:36:33 -0500

    Starting change to Contacts module

--- src/app/contacts/ContactsComponent.js
+++ src/app/contacts/ContactsComponent.js
@@ -1,60 +1,67 @@
-import React from 'react';
-import { Image, Card, Modal, Header, Button } from 'semantic-ui-react';
-import IconizedParagraph from '../common/IconizedParagraph';
-import { FiCopy, FiX } from 'react-icons/fi';
-import contacts from './contacts';
+// import React from 'react';
+// import { Image, Card, Modal, Header, Button } from 'semantic-ui-react';
+// import IconizedParagraph from '../common/IconizedParagraph';
+// import { FiCopy, FiX } from 'react-icons/fi';
+// import contacts from './contacts';
 
-const ContactsComponent = () => {
-  const [departments, setDepartments] = React.useState([]);
-  const [visible, setVisibile] = React.useState(false);
+import React, { useState } from 'react';
+import { Search, Grid, Header, Segment } from 'semantic-ui-react';
 
-  const showModal = () => setVisibile(true);
-  const hideModal = () => setVisibile(false);
+const [isLoading, setLoading] = React.useState(false);
+const [results, setResults] = React.useState([]);
+const [value, setValue] = React.useState('');
 
-  const handleClick = (departments, e) => {
-    setDepartments(departments);
-    showModal();
-  };
+// const ContactsComponent = () => {
+//   const [departments, setDepartments] = React.useState([]);
+//   const [visible, setVisibile] = React.useState(false);
 
-  return (
-    <>
-      <Modal open={visible} size='mini' basic>
-        <Modal.Actions>
-          <p onClick={hideModal} style={{ display: 'flex', alignItems: 'center' }}>
-            >
-            <FiX /> Close
-          </p>
-        </Modal.Actions>
-        <Modal.Content>
-          {departments.map((department) => (
-            <>
-              <Header inverted as='h3'>
-                {department.name}
-              </Header>
-              <IconizedParagraph
-                icon={<FiCopy />}
-                text={department.email}
-                style={{ cursor: 'pointer' }}
-                onClick={() => alert('Copied!')}
-              />
-              <IconizedParagraph
-                icon={<FiCopy />}
-                text={department.phone}
-                style={{ cursor: 'pointer' }}
-                onClick={() => alert('Copied!')}
-              />
-            </>
-          ))}
-        </Modal.Content>
-      </Modal>
-      <Card.Group itemsPerRow={3}>
-        {contacts.map((contact) => (
-          <Card fluid style={{ padding: '1em', cursor: 'pointer' }} onClick={(e) => handleClick(contact.departments)}>
-            <Image src={contact.image} style={{ background: 'white' }} />
-          </Card>
-        ))}
-      </Card.Group>
-    </>
-  );
-};
+//   const showModal = () => setVisibile(true);
+//   const hideModal = () => setVisibile(false);
+
+//   const handleClick = (departments, e) => {
+//     setDepartments(departments);
+//     showModal();
+//   };
+
+//   return (
+//     <>
+//       <Modal open={visible} size='mini' basic>
+//         <Modal.Actions>
+//           <p onClick={hideModal} style={{ display: 'flex', alignItems: 'center' }}>
+//             >
+//             <FiX /> Close
+//           </p>
+//         </Modal.Actions>
+//         <Modal.Content>
+//           {departments.map((department) => (
+//             <>
+//               <Header inverted as='h3'>
+//                 {department.name}
+//               </Header>
+//               <IconizedParagraph
+//                 icon={<FiCopy />}
+//                 text={department.email}
+//                 style={{ cursor: 'pointer' }}
+//                 onClick={() => alert('Copied!')}
+//               />
+//               <IconizedParagraph
+//                 icon={<FiCopy />}
+//                 text={department.phone}
+//                 style={{ cursor: 'pointer' }}
+//                 onClick={() => alert('Copied!')}
+//               />
+//             </>
+//           ))}
+//         </Modal.Content>
+//       </Modal>
+//       <Card.Group itemsPerRow={3}>
+//         {contacts.map((contact) => (
+//           <Card fluid style={{ padding: '1em', cursor: 'pointer' }} onClick={(e) => handleClick(contact.departments)}>
+//             <Image src={contact.image} style={{ background: 'white' }} />
+//           </Card>
+//         ))}
+//       </Card.Group>
+//     </>
+//   );
+// };
 export default ContactsComponent;
